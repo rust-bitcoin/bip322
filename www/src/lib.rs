cb7a1c178ed3e22a7fa6ad711af59665cc327b97
raphjaph <raphjaph@protonmail.com>
2024-08-10 23:11:25 +0200

    Amend

--- www/src/lib.rs
+++ www/src/lib.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn verify(address: &str, message: &str, signature: &str) -> bool {
-  bip322::simple_verify(&address, message, signature).is_ok()
+  bip322::verify_simple_encoded(&address, message, signature).is_ok()
 }
