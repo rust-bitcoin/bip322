4dc6d201fea1ad054f5a6066c6685041a751c88f
raphjaph <raphjaph@protonmail.com>
2024-08-10 02:18:11 +0200

    Amend

--- www/src/lib.rs
+++ www/src/lib.rs
@@ -1,14 +1,6 @@
-use {
-  bitcoin::{address::NetworkUnchecked, Address},
-  wasm_bindgen::prelude::*,
-};
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn verify(address: &str, message: &str, signature: &str) -> bool {
-  let address = address
-    .parse::<Address<NetworkUnchecked>>()
-    .unwrap()
-    .assume_checked();
-
-  bip322::simple_verify(&address, message, signature)
+  bip322::simple_verify(&address, message, signature).is_ok()
 }
