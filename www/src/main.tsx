f251874e9c28255223f6f388e3aa368a5575c109
gmemez <ordinalspractice@gmail.com>
2025-01-17 22:25:06 +0700

    remove theme provider

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { LaserEyesProvider, MAINNET } from "@omnisat/lasereyes";
-import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <LaserEyesProvider config={{ network: MAINNET }}>
-        <App />
-      </LaserEyesProvider>
-    </ThemeProvider>
+    <LaserEyesProvider config={{ network: MAINNET }}>
+      <App />
+    </LaserEyesProvider>
   </StrictMode>
 );
